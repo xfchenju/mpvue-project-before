9d3cc2c29e5f3b8cff181a62027cd3c9f4561be5
DESKTOP-E2UKS9N\97598 <975982556@qq.com>
2019-02-19 14:44:18 +0800

    modify test

--- src/pages/test/index.vue
+++ src/pages/test/index.vue
@@ -1,13 +1,31 @@
 <template>
   <div id="test">
-    <van-cell-group>
+    <div class="mvp-mt-line"></div>
+    <van-cell-group class="mvp-mt">
+      <van-cell
+        title="单元格"
+        value="内容"
+      />
+      <van-cell
+        title="单元格"
+        value="内容"
+      />
+      <van-cell
+        title="单元格"
+        value="内容"
+        :border="false"
+      />
+    </van-cell-group>
+    <div class="mvp-mt-line"></div>
+    <van-cell-group class="mvp-mt">
       <van-field
         :value="username"
+        left-icon="contact"
         label="用户名"
         type="digit"
         placeholder="请输入用户名"
         input-align="right"
-        clearable
+        maxlength="10"
         :error="userErr"
         :error-message="userErrMsg"
         @blur="blurInput"
@@ -20,12 +38,18 @@
           is-link
         />
       </picker>
+      <picker @change="bindPickerChange" :value="date" :range="array">
+        <van-cell
+          title="单元格"
+          :value="date != null ? array[date] : '请选择'"
+          is-link
+        />
+      </picker>
       <van-cell
         title="单元格"
         value="内容"
         :border="false"
       />
-      
     </van-cell-group>
     <div class="mvp-btns-group">
       <van-button custom-class="mvp-main-btn" type="primary" @click="onSubmit" :disabled="isComplate">提交</van-button>
@@ -87,22 +111,49 @@ export default {
       }
     },
     bindPickerChange(e) {
+      console.log(e, 'e')
       this.date = e.mp.detail.value;
+    },
+    bandPickerCancel(e) {
+      console.log(e, 'e')
+      if(this.date == null) {
+        Toast({
+          type: 'warn',
+          message: '请选择！'
+        })
+      }
     }
   }
 }
 </script>
 
-<style>
-.van-button.mvp-main-btn {
-  margin-top: 20rpx;
-  width: 100% !important;
-}
-.mvp-btns-group {
-  margin: 0 auto;
-  width: calc(100% - 60rpx);
-}
-.van-field__error-message {
-  text-align: right !important;
+<style lang="less">
+#test {
+  .mvp-mt-line {
+    width: 100%;
+    height: 20rpx;
+    background: #F8F8F8;
+  }
+  .mvp-mt {
+    margin-top: 20rpx !important;
+    margin-bottom: 20rpx !important;
+    &:after {
+      content: " ";
+      width: 100%;
+      height: 20rpx;
+      background: #F8F8F8;
+    }
+  }
+  .van-button.mvp-main-btn {
+    margin-top: 20rpx;
+    width: 100% !important;
+  }
+  .mvp-btns-group {
+    margin: 0 auto;
+    width: calc(100% - 60rpx);
+  }
+  .van-field__error-message {
+    text-align: right !important;
+  }
 }
 </style>
